fbe12fa905138bd2c8754dab1019ed3a0fdab4a5
ksan09 <kansan060223@gmail.com>
2023-01-09 23:23:58 +0900

    용이랑 비둘기 제작

---
+++ Assets/01.Scripts/Gimmick/Dragon/Dragon.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : PoolableMono
+{
+    [SerializeField] private Transform _dragonBodyParent;
+    private DragonBody[] _dragonBodys;
+
+    private float _speed = 20f;
+
+
+    private void Awake()
+    {
+        _dragonBodys = _dragonBodyParent.GetComponentsInChildren<DragonBody>();
+
+        SetDragonBody();
+    }
+
+    private void FixedUpdate()
+    {
+        DragonMove();
+    }
+
+    private void DragonMove()
+    {
+        transform.position = transform.position + Vector3.up * _speed * Time.fixedDeltaTime;
+    }
+
+    private void SetDragonBody()
+    {
+        for (int i = 0; i < _dragonBodys.Length; i++)
+        {
+            _dragonBodys[i].SetBody(i);
+            _dragonBodys[i].ShakeBody();
+        }
+    }
+    
+
+    public override void Reset()
+    {
+        //
+        SetDragonBody();
+    }
+}
