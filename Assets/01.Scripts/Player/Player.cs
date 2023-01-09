e210e0ce07280a0f16dd3ad567404751d638859e
ehdbs28 <doyun5373@naver.com>
2023-01-09 19:04:09 +0900

    플레이어 기본 움직임

--- Assets/01.Scripts/Player/Player.cs
+++ Assets/01.Scripts/Player/Player.cs
@@ -7,11 +7,35 @@ public class Player : MonoBehaviour
     [SerializeField] private float _fallingSpeed = 1f;
     [SerializeField] private float _moveSpeed = 3f;
 
+    private Rigidbody2D _rigid;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake() {
+        _rigid = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void Update() {
-        Debug.Log(Input.touchCount);
+
+    }
+
+    private void FixedUpdate() {
+        PlayerFall();
+        PlayerMove();
     }
 
     private void PlayerMove(){
-        
+        float input = Input.GetAxisRaw("Horizontal");
+
+        if(input == 1)
+            _spriteRenderer.flipX = true;
+        else if(input == -1)
+            _spriteRenderer.flipX = false;
+
+        _rigid.velocity = new Vector2(input * _moveSpeed, _rigid.velocity.y);
+    }
+
+    private void PlayerFall(){
+        _rigid.velocity = new Vector2(_rigid.velocity.x, -_fallingSpeed);
     }
 }
