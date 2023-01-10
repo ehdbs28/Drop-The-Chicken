8fe4686b8c94a8f930fb9d669d010d4926656170
ehdbs28 <doyun5373@naver.com>
2023-01-10 22:51:15 +0900

    결과 창에 스코어 텍스트 연결

--- Assets/01.Scripts/UI/Screen/ResultScreen.cs
+++ Assets/01.Scripts/UI/Screen/ResultScreen.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ResultScreen : UIScreen
 {
     [SerializeField] private Button tapToRestart;
     [SerializeField] private Button tapToMainmenu;
+
+    [SerializeField] private TextMeshProUGUI currentScore;
+    [SerializeField] private TextMeshProUGUI bestScore;
+
+    public override void UpdateScreenState(bool open)
+    {
+        GameManager.Instance.GetManager<ScoreManager>().ScoreSubscribe(score => currentScore.text = score.ToString("D5"));
+        bestScore.text = GameManager.Instance.GetManager<DataManager>().User.BestScore.ToString("D5");
+
+        base.UpdateScreenState(open);
+    }
 
     public override void Init()
     {
