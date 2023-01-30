fefcf4d2de96be79161a62dff7849ef05fdecabd
도윤 <doyun5373@naver.com>
2023-01-30 16:40:03 +0900

    이동 로직 수정함

--- Assets/01.Scripts/Player/PlayerMove.cs
+++ Assets/01.Scripts/Player/PlayerMove.cs
@@ -1,77 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.EventSystems;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEditor;
+// using UnityEngine;
+// using UnityEngine.EventSystems;
 
-public class PlayerMove : MonoBehaviour
-{
-    Player player;
-    Touch touch;
+// public class PlayerMove : MonoBehaviour
+// {
+//     Player player;
+//     Touch touch;
 
-    private void Awake()
-    {
-        player = GetComponent<Player>();
-    }
+//     private void Awake()
+//     {
+//         player = GetComponent<Player>();
+//     }
 
-    private void Update()
-    {
-        MoveCheck();
-    }
+//     private void Update()
+//     {
+//         MoveCheck();
+//     }
 
-    private void MoveCheck()
-    {
-        if (!player.IsPlay) return;
+//     private void MoveCheck()
+//     {
+//         if (!player.IsPlay) return;
 
-        PC_Move();
-        Moblie_Move();
-    }
+//         PC_Move();
+//         Moblie_Move();
+//     }
 
-    private void PC_Move(){
-        float x = Input.GetAxisRaw("Horizontal");
+//     private void PC_Move(){
+//         float x = Input.GetAxisRaw("Horizontal");
 
-        if(GameManager.Instance.GetManager<PlayerManager>().State == PlayerState.LANDING){
-            if(x != 0) GameManager.Instance.GetManager<PlayerManager>().State = PlayerState.FALLING;
-        }
-        else{
-            Debug.Log(1);
-            if(x != 0)
-            {
-                player.PlayerMove(x > 0);
-            }
-            else
-            {
-                player.StopMove();
-            }
-        }
-    }
+//         if(GameManager.Instance.GetManager<PlayerManager>().State == PlayerState.LANDING){
+//             if(x != 0) GameManager.Instance.GetManager<PlayerManager>().State = PlayerState.FALLING;
+//         }
+//         else{
+//             // if(Input.GetKeyDown(KeyCode.D))
+//             // {
+//             //     player.PlayerMove(x > 0);
+//             // }
+//             // else 
+//             // {
+//             //     player.StopMove();
+//             // }
 
-    private void Moblie_Move(){
-        if(Input.touchCount > 0)
-        {
-            touch = Input.GetTouch(0);
-            Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
+//             // if(Input.GetKeyDown(KeyCode.A)){
+//             //     player.PlayerMove(false);
+//             // }
+//             // else if(Input.GetKeyDown(KeyCode.D)){
+//             //     player.PlayerMove(true);
+//             // }
+//         }
+//     }
 
-            switch (touch.phase)
-            {
-                case TouchPhase.Moved:
-                case TouchPhase.Stationary:
-                    {
-                        if (pos.x > 0)
-                        {
-                            player.PlayerMove(true);
-                        }
-                        else if (pos.x < 0)
-                        {
-                            player.PlayerMove(false);
-                        }
-                    };
-                    break;
-                case TouchPhase.Ended:
-                    player.StopMove();
-                    break;
-            }
+//     private void Moblie_Move(){
+//         if(Input.touchCount > 0)
+//         {
+//             touch = Input.GetTouch(0);
+//             Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
+
+//             switch (touch.phase)
+//             {
+//                 case TouchPhase.Moved:
+//                 case TouchPhase.Stationary:
+//                     {
+//                         if (pos.x > 0)
+//                         {
+//                             player.PlayerMove(true);
+//                         }
+//                         else if (pos.x < 0)
+//                         {
+//                             player.PlayerMove(false);
+//                         }
+//                     };
+//                     break;
+//                 case TouchPhase.Ended:
+//                     player.StopMove();
+//                     break;
+//             }
             
-        }
-    }
-}
+//         }
+//     }
+// }
